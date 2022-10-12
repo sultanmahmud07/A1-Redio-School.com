9bed0397099d89eb75614fa6b7db148667318d4b
sultanmahmud07 <asadkhan01862@gmail.com>
2022-10-12 18:51:54 +0600

    last update

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -14,7 +14,6 @@ const Home = () => {
           </div>
           <div className="right-info w-100 w-lg-50 mt-lg-5">
             <h2 className='home-title'> <span className='title'>Welcome to our school,</span> We are always ready to help you!</h2>
-            <h2 className='home-title'> <span className='title'>Welcome to our school,</span> We are always ready to help you!</h2>
             <p className='home-p'>You will get various opportunities in our school. You will get many types of questions and answers here, you will get a chance to take the exam on any subject. So stay with us and learn.</p>
             <div className="btn-container">
               <button className='home-btn' id='home-btn1'><Link to='/topics'>Topics</Link></button>
