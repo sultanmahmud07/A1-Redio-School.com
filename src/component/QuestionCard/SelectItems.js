691d66170dedbea4f4c9a041dbfe1452c069ae4d
sultanmahmud07 <asadkhan01862@gmail.com>
2022-10-12 21:25:16 +0600

    wrang and rigth detecting tostify added

--- src/component/QuestionCard/SelectItems.js
+++ src/component/QuestionCard/SelectItems.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import './SelectItems.css'
-const SelectItems = ({question}) => {
-  const {id, options, correctAnswer } =question;
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-  // console.log(correctAnswer)
 
-  const correctAnsHandler = (option) => {
+const SelectItems = ({option, question}) => {
+  const {correctAnswer} =question;
+  // console.log(question)
+  // console.log(option)
+
+
+  const selectHeandlerAdd = (option) =>{
+    // console.log(option)
     if(correctAnswer === option){
-      console.log('ha vai paise re')
+      console.log('ok mamu')
+      toast.success('🦄 Wow! Your ans is right!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+
     }
     else{
-      console.log('vai mon kharap koito na ')
+      toast.warn('your answer is wrong!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     }
   }
-
   return (
-    <div onClick={() => correctAnsHandler(options)} className='label-aria cursor-set d-flex p-3 m-3'>
+    <div onClick={() => selectHeandlerAdd(option)} className='label-aria cursor-set d-flex p-3 m-3'>
        <p className=''><input aria-label='label-aria' type="radio" value="Male" name="gender" /></p>
-      <p className='p-text text-start'>{options}</p>
+      <p className='p-text text-start'>{option}</p>
+      <ToastContainer />
     </div>
   );
 };
